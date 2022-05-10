38418eaf532cff62eae55c455e8612a7df6dc1b4
evantaoo <52547607+evantaoo@users.noreply.github.com>
2022-05-10 12:12:44 -0400

    Create cuhspc1.py

---
+++ cuhspc1.py
@@ -0,0 +1,8 @@
+x, y = input().split()
+
+if (x == y):
+	print('Depends on who starts')
+elif (x > y): 
+	print('Arya will think of something else')
+else:
+	print('Margaret will think of something else')
