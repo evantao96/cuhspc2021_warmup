8a5efc8a2dadf9643c27c2bef892031bf99acd34
evantaoo <52547607+evantaoo@users.noreply.github.com>
2022-05-11 19:18:49 -0400

    Update cuhspc1.py

--- problem1/cuhspc1.py
+++ problem1/cuhspc1.py
@@ -3,6 +3,6 @@ x, y = input().split()
 if (x == y):
 	print('Depends on who starts')
 elif (x > y): 
-	print('Arya will think of something else')
-else:
 	print('Margaret will think of something else')
+else:
+	print('Arya will think of something else')
